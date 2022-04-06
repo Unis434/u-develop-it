1786146fe4d5fd9f696648625f50092398d6ede1
Unisa Kamara <unisk434@gmail.com>
2022-04-06 10:19:48 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const db = require('./db/connection');
 const apiRoutes = require('./routes/apiRoutes');
 
-const PORT = process.env.PORT || 3306;
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 // Express middleware
